144a75c551e08b8b233956e94e2a9d9b4bcf8f73
Bharath Kotha <bharathkumar.reddy@vogo.in>
2023-06-29 08:30:06 +0530

    WebRTC conferencing demo in browser

---
+++ WebRTC-web-1/script.js
@@ -0,0 +1,175 @@
+// Variables to store local and remote video elements
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+
+// Variables to store local media stream and WebRTC connection
+let localStream, rtcConnection;
+
+// Variables to store MQTT client and configuration
+const mqttBroker = 'wss://mqtt-dashboard.com:8884/mqtt'; // Replace with your MQTT broker URL
+const mqttTopicPrefix = 'webrtc/conference/'; // Replace with your MQTT topic
+let mqttClient;
+
+// Function to handle MQTT message
+function handleMqttMessage(topic, message) {
+    const payload = JSON.parse(message);
+    const role = document.getElementById('role').value
+
+    // Ignore self messages
+    if(payload.role === role) return;
+
+    // Check if the message is for this meeting ID
+    if (payload.meetingId === document.getElementById('meetingId').value) {
+        if (payload.type === 'sdp') {
+            handleSdpMessage(payload.sdp);
+        } else if (payload.type === 'ice') {
+            handleIceCandidate(payload.candidate);
+        }
+    }
+}
+
+// Function to handle SDP message
+async function handleSdpMessage(sdp) {
+    role = document.getElementById('role').value
+    try {
+        if (role === 'initiator') {
+            await rtcConnection.setRemoteDescription({ type: 'answer', sdp });
+            console.log('SDP answer set as remote description');
+        } else if (role === 'receiver') {
+            await rtcConnection.setRemoteDescription({ type: 'offer', sdp });
+            console.log('SDP offer set as remote description');
+
+            // Create answer and set local description
+            const answer = await rtcConnection.createAnswer();
+            await rtcConnection.setLocalDescription(answer);
+
+            console.log('SDP answer created and set as local description');
+
+            // Send answer to the initiator
+            sendMqttMessage({
+                meetingId: document.getElementById('meetingId').value,
+                type: 'sdp',
+                role: document.getElementById('role').value,
+                sdp: rtcConnection.localDescription.sdp
+            });
+        }
+    } catch (error) {
+        console.error('Error setting remote description:', error);
+    }
+}
+
+// Function to handle ICE candidate message
+async function handleIceCandidate(candidate, role) {
+    try {
+        if (candidate) {
+            await rtcConnection.addIceCandidate(candidate);
+            console.log('ICE candidate added');
+        }
+    } catch (error) {
+        console.error('Error adding ICE candidate:', error);
+    }
+}
+
+// Function to send MQTT message
+function sendMqttMessage(payload) {
+    const meetingId = document.getElementById('meetingId').value;
+    const mqttTopic = mqttTopicPrefix + meetingId
+    mqttClient.publish(mqttTopic, JSON.stringify(payload));
+}
+
+// Function to set up local media
+async function setupLocalMedia() {
+    try {
+        // Request access to user's camera and microphone
+        localStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+
+        // Display local video stream in the local video element
+        localVideo.srcObject = localStream;
+    } catch (error) {
+        console.error('Error accessing local media:', error);
+    }
+}
+
+// Function to create a new WebRTC connection
+function createConnection() {
+    // Create a new WebRTC connection with STUN server configuration
+    const configuration = { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] };
+    rtcConnection = new RTCPeerConnection(configuration);
+
+    // Add local media stream tracks to the connection
+    localStream.getTracks().forEach((track) => {
+        rtcConnection.addTrack(track, localStream);
+    });
+
+    // Set up event listeners for the connection
+
+    // Send SDP offer to the receiver
+    rtcConnection.addEventListener('negotiationneeded', async () => {
+        const role = document.getElementById('role').value;
+        if (role === 'receiver') return;
+        try {
+            // Create offer and set local description
+            const offer = await rtcConnection.createOffer();
+            await rtcConnection.setLocalDescription(offer);
+
+            console.log('SDP offer created and set as local description');
+
+            // Send offer to the receiver
+            sendMqttMessage({
+                meetingId: document.getElementById('meetingId').value,
+                type: 'sdp',
+                role: document.getElementById('role').value,
+                sdp: rtcConnection.localDescription.sdp
+            });
+        } catch (error) {
+            console.error('Error creating SDP offer:', error);
+        }
+    });
+
+    // Handle ICE candidates
+    rtcConnection.addEventListener('icecandidate', (event) => {
+        if (event.candidate) {
+            // Send ICE candidate to the other participant
+            sendMqttMessage({
+                meetingId: document.getElementById('meetingId').value,
+                type: 'ice',
+                role: document.getElementById('role').value,
+                candidate: event.candidate
+            });
+        }
+    });
+
+    // Set remote video stream as the source for the remote video element
+    rtcConnection.addEventListener('track', (event) => {
+        if (event.track.kind === 'video') {
+            remoteVideo.srcObject = event.streams[0];
+        }
+    });
+}
+
+// Function to join the conference
+async function joinConference() {
+    const meetingId = document.getElementById('meetingId').value;
+
+    // Connect to the MQTT broker
+    mqttClient = mqtt.connect(mqttBroker);
+    mqttTopic = mqttTopicPrefix + meetingId
+
+    // Handle MQTT connection
+    mqttClient.on('connect', () => {
+        console.log('Connected to MQTT broker');
+
+        // Subscribe to the MQTT topic
+        mqttClient.subscribe(mqttTopic);
+    });
+
+    // Handle MQTT message
+    mqttClient.on('message', handleMqttMessage);
+
+    // Set up local media and create the WebRTC connection
+    await setupLocalMedia();
+    createConnection();
+}
+
+// Event listener for the "Join Conference" button
+document.getElementById('joinButton').addEventListener('click', joinConference);
